abad17c234ab8319bb8dd8d1c3ba14f1cbc1ff8b
Brian Bystrom <brianbystrom@gmail.com>
2015-03-25 20:35:09 -0400

    First commit, added bootstrap and snapshot.php mockup.

---
+++ snapshot.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class='container'>
+      <div class='row'>
+        <div class='col-md-3 bg-primary'>
+          <div class='row'>
+            <h4>Jennifer Ash</h4>
+            <h5>CUSTOMER SINCE 08/2013</h5>
+            <h5><b>AUTHORIZED USERS:</b></h5>
+            <h6>None</h6>
+            <h5><b>OTHER AT&T SERVICES:</b></h5>
+            <h6>None</h6>
+          </div>
+          <div class='row'>
+            <h5><b>BILLING ACCT NUMBER:</b> 188068967600</h5>
+            <h5><b>ACCT TYPE:</b> IRU</h5>
+            <h5><b>ONLINE ACCT:</b> Yes</h5>
+            <h5><b>PAYMENT TYPE:</b> AutoPay DIRECT DEBIT</h5>
+            <h5><b>BILLING TYPE:</b> Paper</h5>
+          </div>
+          <div class='row'>
+            <h5><b>INSTALLMENT PLANS:</b></h5>
+          </div>
+          <div class='row'>
+            <h5><b>LAST BILL AMOUNT:</b></h5>
+            <h4>$0.00</h4>
+            <h6>Bill Due Date: 03/22/2015</h6>
+            <h5><b>LAST ADJUSMENT:</b></h5>
+            <h6>None</h6>
+            <button type="button" class="btn btn-info">View Account Details</button>
+          </div>
+        </div>
+
+        <div class='col-md-9'>
+          <div class='row bg-primary'>
+              <h4>Contacts in last 30 days (0)</h4>
+          </div>
+          <div class='row'>
+            timeline goes here. <br><br>
+          </div>
+          <div class='col-md-9'>
+              <div class='row bg-primary'>
+                <h4>Devices (1)</h4>
+                <h6>Device selection can display up to 15 devices.</h6>
+              </div>
+              <div class='row'>
+                  <div class='col-md-5'>
+                    <h5>Samsung Galaxy S4</h5>
+                    <img src='http://ecx.images-amazon.com/images/I/41Lq6GIxQ1L._SL500_AA300_.jpg' style='max-height: 100px; max-width: 100px;'></img>
+                    <h6><b>Android</b></h6>
+                    <h6><b>CTN: 231-620-0919</b></h6>
+                    <h6><b>IMEI: 356567056814732</b></h6>
+                    <h6><b>Contract Ends:</b> 08/27/2015</h6>
+                  </div>
+
+                  <div class='col-md-7'>
+                      <h5>Nation 450 with Rollover</h5>
+                      <h6>Billing Cycle</h6>
+                      <h6>03/01/2015-03/28/2015</h6>
+                      <h5>Meter</h5>
+                      <h6>Talk</h6>
+                      <h5>Meter</h5>
+                      <h6>Messaging</h6>
+                      <h5>Meter</h5>
+                      <h6>Data</h6>
+                      <h5>Meter</h5>
+                      <h5>View Usage Details</h5>
+                  </div>
+              </div>
+              <div class='row'>
+                  <div class='col-md-3'>
+                      <h5>Upgrade Eligible</h5>
+                      <h5>Yes</h5>
+                      <h5>View All Devices</h5>
+                  </div>
+                  <div class='col-md-3'>
+                      <h5>Standard</h5>
+                      <h5>Early</h5>
+                      <h5>Installment</h5>
+                      <h5>Shared</h5>
+                  </div>
+                  <div class='col-md-3'>
+                      <h5>Warranty Expired</h5>
+                      <h5>09/09/2014</h5>
+                  </div>
+                  <div class='col-md-3'>
+                      <h5>Insurance Coverage</h5>
+                      <h5>Early</h5>
+                  </div>
+              </div>
+            </div>
+            <div class='col-md-3'>
+              <div class='row'>
+                <h4>Device Notices (13)</h4>
+                <h5>Line Information</h5>
+                <h5>User Reported Network Info</h5>
+                <h5>Device Alerts</h5>
+              </div>
+              <div class='row'>
+                <h4>Account Notices (1)</h4>
+                <h5>Open Items</h5>
+                <h5>Outbound Offers</h5>
+              </div>
+            </div>
+
+
+        </div>
+
+      </div>
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
